6cb2638af37076a3eaf23735f4f31c9a462c99eb
Laxman Anand <laxman7291@gmail.com>
2025-07-04 13:00:14 +0530

    logo_setup

--- src/@pages_v2/Auth.jsx
+++ src/@pages_v2/Auth.jsx
@@ -3,21 +3,23 @@ import FeaturesComponent from "../components/registration/FeaturesComponent";
 
 export const Auth = ({ children }) => {
   return (
-    <div
-      className="d-flex w-100 gap-4"
-      style={{
-        minHeight: "100vh",
-        overflowY: "auto",
-      }}
-    >
-      {/* Placeholder for features component */}
-      <div
-        style={{
-          width: "60%",
-        }}
-        className="auth-left d-flex justify-content-center align-items-center flex-column gap-5 zoqq-sign-up-1-1"
-      >
-        {/* <img src="/cover/auth-cover.svg" alt="" className="w-40" />
+    <>
+      <div>
+        <div
+          className="d-flex w-100 gap-4"
+          style={{
+            minHeight: "100vh",
+            overflowY: "auto",
+          }}
+        >
+          {/* Placeholder for features component */}
+          <div
+            style={{
+              width: "50%",
+            }}
+            className="position relative auth-left d-flex justify-content-center align-items-center flex-column gap-5 zoqq-sign-up-1-1"
+          >
+            {/* <img src="/cover/auth-cover.svg" alt="" className="w-40" />
         <div className="w-75">
           <h1 className="text-uppercase fw-bold">
             Stay on top of all company spending in real-time
@@ -27,11 +29,13 @@ export const Auth = ({ children }) => {
           </h5>
         </div> */}
 
-        <FeaturesComponent />
-      </div>
+            <FeaturesComponent />
+          </div>
 
-      {/* Placeholder for auth child components */}
-      <div className="auth-form">{children}</div>
-    </div>
+          {/* Placeholder for auth child components */}
+          <div className="auth-form">{children}</div>
+        </div>
+      </div>
+    </>
   );
 };
